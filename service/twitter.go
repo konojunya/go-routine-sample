ac37d6e89ddf97d7a2f0bba87ac5c8933418895a
konojunya <junya03dance@gmail.com>
2018-02-26 10:30:58 +0900

    twitter accessの部分にtime.Sleep

--- service/twitter.go
+++ service/twitter.go
@@ -2,12 +2,15 @@ package service
 
 import (
 	"fmt"
+	"time"
 )
 
 var count = 0
 
 // PostTwitterAPI twitter apiっぽい疑似アクセス
 func PostTwitterAPI() ([]string, bool) {
+	time.Sleep(3 * time.Second)
+
 	fmt.Println("twitter access!")
 	count++
 	if count > 10 {
